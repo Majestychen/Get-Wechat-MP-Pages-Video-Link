94a204b9c6a8990b738ba43f38acb3a7f7d55563
CRH380A-2722 <609657831@qq.com>
2016-09-27 23:10:53 +0800

    自动加载必要的脚本

---
+++ script-autoloader.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *	微信公众号文章内嵌视频链接探查器 - 自动加载includes目录里面的PHP脚本
+ *
+ *	@author CRH380A-2722 <609657831@qq.com>
+ *	@copyright 2016 CRH380A-2722
+ *	@license https://raw.githubusercontent.com/CRH380A-2722/Get-Wechat-MP-Pages-Video-Link/master/LICENSE MIT
+ */
+
+require './includes/simple_html_dom.php';
+require './includes/functions.php';
