5634a91b39023fcb8dd1b64d47846e24bba2efd3
CRH380A-2722 <609657831@qq.com>
2016-09-27 23:14:29 +0800

    模板文件

---
+++ templates/footer.php
@@ -0,0 +1,16 @@
+		</div>
+
+		<div class="footer">
+			<p>&copy; 2016 CRH380A-2722.</p>
+		</div>
+
+		<!-- Scripts -->
+		<script src="./source/js/jquery.min.js"></script>
+		<script src="./source/js/bootstrap.min.js"></script>
+		<script src="./source/js/jquery.pjax.min.js"></script>
+		<script src="./source/js/nprogress.min.js"></script>
+		<script src="./source/js/clipboard.min.js"></script>
+		<script src="./source/js/custom.min.js"></script>
+	</body>
+</html>
+<?php ob_end_flush(); ?>
