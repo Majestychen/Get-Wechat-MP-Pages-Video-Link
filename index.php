e47c87e7b6db7dddf38e4b06ed7f7e5a6e453ab8
CRH380A-2722 <609657831@qq.com>
2016-09-27 23:10:35 +0800

    首页

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *	微信公众号文章内嵌视频链接探查器 - 首页
+ *
+ *	@author CRH380A-2722 <609657831@qq.com>
+ *	@copyright 2016 CRH380A-2722
+ *	@license https://raw.githubusercontent.com/CRH380A-2722/Get-Wechat-MP-Pages-Video-Link/master/LICENSE MIT
+ */
+
+require './script-autoloader.php';
+
+template('header', 'form', 'footer');
